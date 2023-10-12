87e3f959639252626cb7d2c37150e1e0a8585124
Neemias Vieira <neemias.vieira@live.com>
2023-10-12 20:47:13 -0300

    Correção em timezone

--- src/assets/DateSaoPaulo.js
+++ src/assets/DateSaoPaulo.js
@@ -6,7 +6,12 @@ export class MyDate extends Date {
 
     adjustTimezone() {
         const offset = this.getTimezoneOffset();
-        const offsetMilliseconds = offset * 60 * 1000; // Converte minutos em milissegundos
+        const offsetMilliseconds = offset * 60 * 1000; 
         this.setTime(this.getTime() - offsetMilliseconds);
+    }
+
+    toLocaleString() {
+        const options = { timeZone: 'America/Sao_Paulo', hour12: false };
+        return super.toLocaleString('pt-BR', options);
     }
 }
