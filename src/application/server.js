5c39042f0e5e0a181a2c290cd7630f0f4ced7d14
Neemias Vieira <neemias.vieira@live.com>
2023-10-31 15:32:00 -0300

    Documentação finalizada

--- src/application/server.js
+++ src/application/server.js
@@ -1,41 +1,59 @@
 import express from "express";
 import routes from "./routes.js";
 import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "../swagger.json" assert { type: "json" };
 import { logs } from "../middlewares/logs.js";
 import { ErrosComuns } from "../middlewares/erros.js";
-import cron from 'node-cron';
+import cron from "node-cron";
 import prisma from "../database/prisma/prismaClient.js";
 
-// https://sms-api-git-main-neemiasvieira.vercel.app/ 
+const SwaggerOptions = {
+  customSiteTitle: "SMS-API",
+  // customfavIcon: "../assets/img/favicon",
+
+  //Temas do Swagger: https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/
+  customCssUrl:
+    "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-outline.css",
+  customCss: ".swagger-ui .topbar { background-color: rgb(16, 187, 64) } !important"
+};
+
+// https://sms-api-git-main-neemiasvieira.vercel.app/
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: "*",
-  methods: ['GET', 'POST', "DELETE", "PATCH", "PUT", "OPTIONS"],
-  allowedHeaders: "*",
-  credentials: true,
-  optionsSuccessStatus: 200,
-  exposedHeaders: "*",
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
+    allowedHeaders: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
+    exposedHeaders: "*",
+  })
+);
 
-}));
-
-cron.schedule('0 0 * * *', async() => {
-  console.log("Exclusão dos logs pela varredura da meia noite concluido com sucesso 😎");
-  await prisma.logs.deleteMany();
-}, {
-  timezone: "America/Sao_Paulo"
-});
+cron.schedule(
+  "0 0 * * *",
+  async () => {
+    console.log(
+      "Exclusão dos logs pela varredura da meia noite concluido com sucesso 😎"
+    );
+    await prisma.logs.deleteMany();
+  },
+  {
+    timezone: "America/Sao_Paulo",
+  }
+);
 
 app.use(logs);
 app.use(routes);
-app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec, SwaggerOptions));
 app.use(ErrosComuns);
 
-app.listen(process.env.PORT || 3333,"0.0.0.0", () => {
+app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
   console.log("▶️  Servidor iniciado com sucesso em http://localhost:3333 🆙");
-})
-
+});
