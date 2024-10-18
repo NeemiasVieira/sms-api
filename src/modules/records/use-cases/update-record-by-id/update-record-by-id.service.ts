ec2620ebc7a4a6753991241867a1cdd493899713
Neemias Vieira <neemias.vieira@live.com>
2024-10-18 14:16:41 -0300

    one time execute removed

--- src/modules/records/use-cases/update-record-by-id/update-record-by-id.service.ts
+++ src/modules/records/use-cases/update-record-by-id/update-record-by-id.service.ts
@@ -1,22 +1,21 @@
-import { Injectable } from "@nestjs/common";
-import { GraphQLError } from "graphql";
-import { PrismaService } from "src/database/prisma/prisma.service";
-import { ValidationsService } from "src/utils/validations.service";
-import { Record } from "../../record.type";
-import { IUpdateRecordArgs } from "./update-record-by-id.types";
+import { Injectable } from '@nestjs/common';
+import { GraphQLError } from 'graphql';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { ValidationsService } from 'src/utils/validations.service';
+import { Record } from '../../record.type';
+import { IUpdateRecordArgs } from './update-record-by-id.types';
 
 @Injectable()
 export class UpdateRecordByIdService {
   constructor(
     private readonly validationsService: ValidationsService,
-    private readonly prismaService: PrismaService,
+    private readonly prismaService: PrismaService
   ) {}
 
   async updateRecord(args: IUpdateRecordArgs): Promise<Record> {
     const { id, usuario, ...data } = args;
 
-    if (!this.validationsService.isObjectId(id))
-      throw new GraphQLError("ID invalido!");
+    if (!this.validationsService.isObjectId(id)) throw new GraphQLError('ID invalido!');
 
     await this.prismaService.$connect();
 
@@ -24,21 +23,20 @@ export class UpdateRecordByIdService {
       where: { id, dataDeExclusao: null },
     });
 
-    if (!registro) throw new GraphQLError("Registro não encontrado");
+    if (!registro) throw new GraphQLError('Registro não encontrado');
 
     const planta = await this.prismaService.plantas.findUnique({
       where: { id: registro.idPlanta, dataDeExclusao: null },
     });
 
-    if (!planta) throw new GraphQLError("Planta não encontrada");
+    if (!planta) throw new GraphQLError('Planta não encontrada');
 
-    if (planta.idDono !== usuario.id)
-      throw new GraphQLError("Usuário não autorizado");
+    if (planta.idDono !== usuario.id) throw new GraphQLError('Usuário não autorizado');
 
     const atualizacaoNaoAutorizada = !(planta.simulado && registro.simulado);
 
     if (atualizacaoNaoAutorizada) {
-      throw new GraphQLError("Só é possível atualizar registros simulados");
+      throw new GraphQLError('Só é possível atualizar registros simulados');
     }
 
     const registroAtualizado = await this.prismaService.registros.update({
--- src/utils/executeOneTime.resolver.ts
+++ src/utils/executeOneTime.resolver.ts
@@ -9,20 +9,6 @@ export class ExecuteOneTimeResolver {
   async executeOneTime() {
     await this.prismaService.$connect();
 
-    const registros = await this.prismaService.registros.findMany();
-
-    registros.forEach(async (registro) => {
-      const especie = await this.prismaService.especies.findFirst({ where: { nome: registro.nomeEspecie } });
-      await this.prismaService.registros.update({
-        where: {
-          id: registro.id,
-        },
-        data: {
-          idEspecie: especie.id,
-        },
-      });
-    });
-
     await this.prismaService.$disconnect();
 
     return 'Executado com sucesso.';
